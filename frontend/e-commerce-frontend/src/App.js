8c80eb018cec1b554efa8ed1c702bbaf347a857c
Miroslav Chushkov <149536011+Miroslav981@users.noreply.github.com>
2023-11-08 20:32:51 +0200

    frontend init

---
+++ frontend/e-commerce-frontend/src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+import Layout from './components/Layout';
+import NoPage from './pages/NoPage';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
